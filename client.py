8070b6842485d99ef6755430faed6dd8acad4bea
Alex <kelheor23@gmail.com>
2025-01-06 02:33:31 +0300

    simple exchange client to server

--- client.py
+++ client.py
@@ -1,5 +1,37 @@
+import socket
+import time
+import random
+
+import globals
+import msg_pb2
+
+countMsg = 0
+
+def createMsg():
+    global countMsg
+    msg = msg_pb2.SensorData()
+
+    msg.device_id   = 0
+    msg.event_id    = countMsg
+    msg.humidity    = random.uniform(30.0, 50.0)
+    msg.temperature = random.uniform(10.0, 30.0)
+
+    countMsg += 1
+    return msg
+
 def client():
-    print("Подключение к серверу...")
+    while True:
+        try:
+            print("Подключение к серверу...")
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
+                soc.connect(globals.SERVER_ADDRESS)
+                while True:
+                    soc.send(createMsg().SerializeToString())
+                    time.sleep(1)  # Ждем 1 секунду
+
+        except Exception as e:
+            print(f"Ошибка подключения: {e}")
+            time.sleep(1)
 
 if __name__ == '__main__':
     client()
--- server.py
+++ server.py
@@ -1,5 +1,29 @@
+import socket
+
+import globals
+
 def server():
-    print("Запуск сервера")
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(globals.SERVER_ADDRESS)  # Привязываем сервер к адресу
+    server_socket.listen(1)
+
+    print("Сервер запущен")
+
+    while True:
+        conn, addr = server_socket.accept()  # Принимаем входящее соединение
+        print(f"Подключился клиент {addr}")
+
+        with conn:
+            try:
+                while True:
+                    data = conn.recv(1024)  # Принимаем входящее сообщение. Размер буффера 1 КБ
+                    if not data:
+                        break;
+                    print(data)                    
+            except Exception as e:
+                print(f"Что-то пошло не так: {e}")
+        
+        print(f"Клиент ушел {addr}")
 
 if __name__ == '__main__':
     server()
