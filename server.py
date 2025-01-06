43e686076fb7a6ced16f6145a358126c7cc1c747
Alex <kelheor23@gmail.com>
2025-01-06 04:36:29 +0300

    write to DB

--- server.py
+++ server.py
@@ -1,7 +1,18 @@
 import socket
+import influxdb_client, os, time
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
 
 import globals
 import msg_pb2
+
+token = "bJiwAUAcziGUXcMPiwwDvKm5Dni847YspdXETTuT1CVVvmbLgKvdQ1anOcBURIjAilFF1flpiCBMHhM0ubbF2g=="
+org = "temp"
+url = "http://localhost:8086"
+bucket="Sensors"
+
+write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+write_api = write_client.write_api(write_options=SYNCHRONOUS)
 
 # Функция чтения данных, заодно выводим в терминал
 def readData(protobufData):
@@ -14,6 +25,20 @@ def readData(protobufData):
     print(f"Температура: {msg.temperature:.2f}°C")
     print()
     return msg
+
+# Запись в БД
+def writeMsgToDB(msg):
+    try:
+        point = Point("SensorData") \
+            .tag("device_id", msg.device_id) \
+            .field("event_id", msg.event_id) \
+            .field("humidity", msg.humidity) \
+            .field("temperature", msg.temperature)
+    
+        write_api.write(bucket=bucket, org="temp", record=point)
+        print("Данные записаны")
+    except Exception as e:
+        print(f"В момент записи в БД что-то пошло не так: {e}")
 
 def server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -32,7 +57,8 @@ def server():
                     data = conn.recv(1024)  # Принимаем входящее сообщение. Размер буффера 1 КБ
                     if not data:
                         break
-                    readData(data)                
+                    msg = readData(data) # Парсим пришедшее сообщение
+                    writeMsgToDB(msg)    # Записываем сообщдение БД   
             except Exception as e:
                 print(f"Что-то пошло не так: {e}")
         
